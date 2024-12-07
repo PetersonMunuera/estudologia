53125a38dc7e8d355141d6f0d9140249681dd702
Peterson Munuera <peterson.munuera@gmail.com>
2024-12-07 14:34:51 -0300

    fonts and colors configured

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,26 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        inter: ['var(--font-inter)'],
+        chivo: ['var(--font-chivo)'],
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        ["primary-gradient"]: "var(--purple-gradient)",
+        ["primary-100"]: "var(--purple-100)",
+        ["purple-500"]: "var(--purple-500)",
+        ["gray-100"]: "var(--gray-100)",
+        ["gray-200"]: "var(--gray-200)",
+        ["gray-300"]: "var(--gray-300)",
+        ["gray-500"]: "var(--gray-500)",
+        ["gray-600"]: "var(--gray-600)",
+        ["green-100"]: "var(--green-100)",
+        ["green-500"]: "var(--green-500)",
+        ["yellow-100"]: "var(--yellow-100)",
+        ["yellow-500"]: "var(--yellow-500)",
+        ["red-500"]: "var(--red-500)",
       },
     },
   },
